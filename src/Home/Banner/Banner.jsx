b55e705a5fbee315ee814f35c6177dc1349ff4b3
Shariful4447 <shariful.islam4447@gmail.com>
2024-04-14 23:41:55 +0200

    Banner paralox image design complete

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -1,12 +1,14 @@
 
+import bannerImage1 from '../../assets/paralox-image.jpeg'
+import bannerImage2 from '../../assets/paralox-image1.jpeg'
 const Banner = () => {
     return (
         <div>
             <div className="hero min-h-screen bg-base-200" style={{backgroundImage: 'url(https://i.ibb.co/DzvZDy5/home-bg.png)'}}>
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="flex gap-5">
-                        <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="h-[500px] w-[300px] rounded-lg shadow-2xl" />
-                        <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="h-[500px] w-[300px] rounded-lg shadow-2xl my-16" />
+                        <img src={bannerImage1} className="h-[600px] w-[300px] rounded-full shadow-2xl" />
+                        <img src={bannerImage2} className="h-[600px] w-[300px] rounded-full shadow-2xl my-16" />
                     </div>    
                     <div>
                         <h1 className="text-7xl font-bold">Adventure &</h1>
