ec8fba5404b50aa69d6b6e35e21fba7e7ec39554
Shariful4447 <shariful.islam4447@gmail.com>
2024-04-14 22:58:10 +0200

    Banner bg added

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -1,18 +1,17 @@
 
-import ParallaxComponent from 'react-parallax-component';
 const Banner = () => {
     return (
         <div>
-            <ParallaxComponent
-                speed="0.003"
-                width="300"
-                top="40%"
-                left="100"
-                >
-                <div>
-                    Children component
+            <div className="hero min-h-screen bg-base-200" style={{backgroundImage: 'url(https://i.ibb.co/DzvZDy5/home-bg.png)'}}>
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button className="btn btn-primary">Get Started</button>
+                    </div>
                 </div>
-            </ParallaxComponent>
+            </div>
             
         </div>
     );
