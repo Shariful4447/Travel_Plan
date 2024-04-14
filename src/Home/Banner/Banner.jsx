9a1653ebb7ffb2e319333cc00f68531a54c8fc93
Shariful4447 <shariful.islam4447@gmail.com>
2024-04-14 23:14:10 +0200

    Banner relative pic added but need to resize

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -4,11 +4,14 @@ const Banner = () => {
         <div>
             <div className="hero min-h-screen bg-base-200" style={{backgroundImage: 'url(https://i.ibb.co/DzvZDy5/home-bg.png)'}}>
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+                    <div className="flex gap-5">
+                        <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="h-[500px] w-[300px] rounded-lg shadow-2xl" />
+                        <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="h-[500px] w-[300px] rounded-lg shadow-2xl my-16" />
+                    </div>    
                     <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                        <h1 className="text-7xl font-bold">Adventure &</h1>
+                        <p className="py-6 text-7xl font-bold">Experience The Travel !</p>
+                        <button className="btn btn-primary">Get Started</button>
                     </div>
                 </div>
             </div>
