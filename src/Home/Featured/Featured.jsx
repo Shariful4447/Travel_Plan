3196198cd9b4a8f3ead06d60275c0b389a4e5032
Shariful4447 <shariful.islam4447@gmail.com>
2024-04-16 00:55:26 +0200

    Featured section added

---
+++ src/Home/Featured/Featured.jsx
@@ -0,0 +1,31 @@
+
+
+const Featured = () => {
+    return (
+        <div className="mt-5 mb-5 hero" style={{backgroundImage: 'url(https://i.ibb.co/DzvZDy5/home-bg.png)'}}>
+            <div className=" hero-overlay bg-opacity-80"></div>
+            <div>
+                <p className="text-2xl text-white">Dream Vacation Destination</p>
+                <p className="text-7xl font-bold">Plan the Trip of a Lifetime <br /> with Ease</p>
+                <p className="text-xl mt-5 text-[#1BBC9B] font-bold">Whether you're looking for a romantic getaway, a family-friendly adventure, or a
+                    solo <br /> journey to explore the world, a travel agency can provide you with a <br />
+                    custom-tailored itinerary that exceeds your expectations.
+                </p>
+                <button className="btn btn-success text-xl mt-5 mb-5">More Info</button>
+            </div>
+            <div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Featured;
