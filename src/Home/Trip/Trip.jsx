629df64375b10195e29922ce94cfc09095caaec9
Shariful4447 <shariful.islam4447@gmail.com>
2024-04-17 03:06:11 +0200

    image updated in the featured trip section

--- src/Home/Trip/Trip.jsx
+++ src/Home/Trip/Trip.jsx
@@ -1,10 +1,12 @@
 
-
+import trip1 from '../../assets/egypt.jpeg'
+import trip2 from '../../assets/greece.jpeg'
+import trip3 from '../../assets/france.jpeg'
 const Trip = () => {
     return (
         <div className="mb-5 grid grid-cols-3 gap-5 place-items-center">
             <div className="card w-[500px] bg-base-100 shadow-xl">
-                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                <figure><img src={trip1} alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">Shoes!</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
@@ -14,7 +16,7 @@ const Trip = () => {
                 </div>
             </div>
             <div className="card [500px] bg-base-100 shadow-xl">
-                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                <figure><img src={trip2} alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">Shoes!</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
@@ -24,12 +26,19 @@ const Trip = () => {
                 </div>
             </div>
             <div className="card [500px] bg-base-100 shadow-xl">
-                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                <figure><img src={trip3} alt="Shoes" /></figure>
+                <div>
+                    <div><p>icon</p></div>
+                    <div><p>details</p></div>
+
+                </div>
                 <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <h2 className="card-title">Chiang Mai</h2>
+                    <p>Visit the temples and the Chiang Mai Night
+Bazaar, a huge huge market located on
+Chiang Klan Road.</p>
                     <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <button className="btn btn-primary">Details</button>
                     </div>
                 </div>
             </div>
