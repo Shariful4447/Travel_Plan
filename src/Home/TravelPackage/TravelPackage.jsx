9b0c5027be3afdcd0a455e23a84b16540137544e
Shariful4447 <shariful.islam4447@gmail.com>
2024-05-01 23:01:45 +0200

    subscription issues with flex-grow

--- src/Home/TravelPackage/TravelPackage.jsx
+++ src/Home/TravelPackage/TravelPackage.jsx
@@ -11,7 +11,7 @@ const TravelPackage = () => {
                 <h2 className="text-xl">We believe that everyone deserves to experience their dream vacation without breaking the bank. <br />
                 Thats why we offer a price section on our website that features a range of affordable travel</h2>
                 <div className="flex gap-5 mt-5 place-content-center">
-                    <div className="border-2 bg-[#F2F2F2]">
+                    <div className="border-2 bg-[#F2F2F2] flex-none">
                         <div className="p-5">
                             <p>Half Board</p>
                             <h2>$ 50 Day/2</h2>
@@ -23,25 +23,29 @@ const TravelPackage = () => {
                             <button className="btn btn-success">View Trips</button>
                         </div>
                     </div>
-                    <div>
-                        <p>All Inclusive</p>
-                        <h2>$ 32 Day/2</h2>
-                        <p>Meals in Restaurants</p>
-                        <p>Alcoholic beverages</p>
-                        <p>Minimum 3 Star Hotel</p>
-                        <p>Transfers from Airport</p>
-                        <p>Incl. Museum Tickets</p>
-                        <button className="btn btn-active">View Trips</button>
+                    <div className="border-2 grow h-[100px] w-[180px] bg-[#1BBC9B] shadow-lg">
+                        <div className="p-5">
+                            <p>All Inclusive</p>
+                            <h2>$ 32 Day/2</h2>
+                            <p>Meals in Restaurants</p>
+                            <p>Alcoholic beverages</p>
+                            <p>Minimum 3 Star Hotel</p>
+                            <p>Transfers from Airport</p>
+                            <p>Incl. Museum Tickets</p>
+                            <button className="btn btn-active">View Trips</button>
+                        </div>
                     </div>
-                    <div>
-                        <p>Excursions Included</p>
-                        <h2>$ 68 Day/2</h2>
-                        <p>Minimum 3 Star Hotel</p>
-                        <p>Meals in Restaurants</p>
-                        <p>Incl. Museum Tickets</p>
-                        <p>Meals in Restaurants</p>
-                        <p>Alcoholic beverages</p>
-                        <button className="btn btn-success">View Trips</button>
+                    <div className="border-2 bg-[#434A54] flex-none">
+                        <div className="p-5">
+                            <p>Excursions Included</p>
+                            <h2>$ 68 Day/2</h2>
+                            <p>Minimum 3 Star Hotel</p>
+                            <p>Meals in Restaurants</p>
+                            <p>Incl. Museum Tickets</p>
+                            <p>Meals in Restaurants</p>
+                            <p>Alcoholic beverages</p>
+                            <button className="btn btn-success">View Trips</button>
+                        </div>
                     </div>
                 </div>
 
