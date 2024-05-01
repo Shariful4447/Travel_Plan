f13961fe0bec739dd22e1d5d89d4a7f70210912b
Shariful4447 <shariful.islam4447@gmail.com>
2024-05-01 22:38:36 +0200

    subscription section added

--- src/Home/TravelPackage/TravelPackage.jsx
+++ src/Home/TravelPackage/TravelPackage.jsx
@@ -10,6 +10,25 @@ const TravelPackage = () => {
                 <h2 className="text-5xl m-2">Affordable Travel Packages</h2>
                 <h2 className="text-xl">We believe that everyone deserves to experience their dream vacation without breaking the bank. <br />
                 Thats why we offer a price section on our website that features a range of affordable travel</h2>
+                <div>
+                    <div>
+                        <p>Half Board</p>
+                        <h2>$ 50 Day/2</h2>
+                        <p>Transfers from Airport</p>
+                        <p>Minimum 3 Star Hotel</p>
+                        <p>Incl. Museum Tickets</p>
+                        <p>Alcoholic beverages</p>
+                        <p>Meals in Restaurants</p>
+                        <button className="btn btn-success">View Trips</button>
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+
             </div>
 
         </div>
